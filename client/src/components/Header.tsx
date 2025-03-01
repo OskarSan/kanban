4509beaa873ccdaffe57b4a81afe57f682c6a794
OskarSan <oskar.sankila@gmail.com>
2025-03-01 22:00:49 +0200

    searchbar look

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, TextField } from '@mui/material';
 import { styled } from '@mui/system';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
@@ -55,13 +55,45 @@ const Header: React.FC = () => {
                     <Typography variant="h6" style={{ flexGrow: 1 }}>
                         Kanban Board
                     </Typography>
+                    
                     <Button className="buttonSpacing" color="inherit" onClick={handleHomeClick}>Home</Button>
-                    <Button className="buttonSpacing" color="inherit">About</Button>
-
                     {token ? (
-                        <LogoutButton onClick={handleLogoutClick}>Sign out</LogoutButton>
+                        <>
+                            <TextField
+                            className="buttonSpacing searchBar" 
+                            id="outlined-basic" 
+                            label="Search" 
+                            variant="outlined" 
+                            placeholder='Search' 
+                            sx={{
+                                marginRight: '10px',
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Text color
+                                },
+                                '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {
+                                        borderColor: 'white', // Outline color
+                                    },
+                                    '&:hover fieldset': {
+                                        borderColor: 'white', // Outline color on hover
+                                    },
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: 'white', // Outline color when focused
+                                    },
+                                },
+                                '& .MuiInputLabel-root': {
+                                    color: 'white', // Label color
+                                },
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: 'white', // Label color when focused
+                                },
+                            }}
+                            />
+                            <LogoutButton className="buttonSpacing" onClick={handleLogoutClick}>Sign out</LogoutButton>
+                        </>
+                        
                     ) : (
-                        <LoginButton onClick={handleLoginClick}>Sign in</LoginButton>
+                        <LoginButton className="buttonSpacing" onClick={handleLoginClick}>Sign in</LoginButton>
                     )}
 
                 
